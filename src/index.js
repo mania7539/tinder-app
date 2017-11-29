d37d84a97bc2e8a746b86c1828c942ad28635fa7
David.Rz.Liu <mania7539@gmail.com>
2017-11-29 18:39:13 +0800

    clean to default app structure

--- src/index.js
+++ src/index.js
@@ -1,64 +1,14 @@
-import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import YoutubeSearch from "youtube-api-search";
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
-
-const API_KEY = "AIzaSyDfhiV7sDt5xpP9sjxmJN6ZWNyEE7oW4hI";
 
 // Create a new component. 
 // This component will produce some HTML
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        }
-        /* A list of videos, so use an array is very reasonable */
-
-        this.videoSearch("surfboards");
-
-    }
-
-    videoSearch(term) {
-        YoutubeSearch({ key: API_KEY, term: term }, (videos) => {
-            console.log(videos);
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-            // add the concept of selected video.
-            //
-            // this.setState({ videos });
-            // in ES6, the above code is equal to 
-            // this.setState({ videos: videos });
-        });
-        /* Youtube Search: pass an JSON object with a search term and the API key */
-        /* Will get 5 youtube search results, including thumbnails, title, description, on console log */
-        /* The 2nd argument is the callback function */
-    }
-
-    render() {
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
-        // _.debounce will add latency to fire off this function
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-                    videos={this.state.videos} />
-            </div>
-        );
-        /* onVideoSelected is a custom function to props (pass a function to next level component), which has an argument selectedVideo */
-        /* can't pass <VideoDetail video={this.state.videos[0]} /> directly, since index 0 is undefined when it first loaded. */
-        /* passing 'state' is just like passing 'props' */
-    }
+const App = () => {  
+    return (
+        <div>
+            my default app
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
