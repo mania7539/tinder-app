d80b734d2ff0f982e29d0d466c68b9f1fed213fd
David.Rz.Liu <mania7539@gmail.com>
2017-11-29 18:50:03 +0800

    create a reducer to generate value pieces and use combineReducers to map them with state

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'Harry Poter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' },
+    ]
+}
+
+/* Reducer is just a function which generates value pieces of state */
