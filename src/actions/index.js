3d67394ea008c378ad55a9286f946a05bd6fe2b7
David.Rz.Liu <mania7539@gmail.com>
2017-11-30 18:09:15 +0800

    create an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 export function selectBook(book) {
     console.log("A book has been selected: ", book.title);
 
+    // selectBook is an action creator, it needs to return an action,
+    //      an object with a type property
+    // actions usually have 2 values: type and payload,
+    //      type is for describing the purpose of the action,
+    //      payload further describes or clarifies the conditions of the action that's being triggered
+    return ({
+        type: "BOOK_SELECTED",
+        payload: book
+    });
+
 }
 /** 
  * We actually have to make sure that 
